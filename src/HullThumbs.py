46e0f56dcfd693ee1a680f046f8f09897ae0ba5f
Bugge T. Jensen <btj@force.dk>
2024-05-28 10:09:06 +0200

    Calculation of hull damping curves added

--- src/HullThumbs.py
+++ src/HullThumbs.py
@@ -1,356 +1,121 @@
 
+import numpy as np
+from scipy.constants import g
 import Thumbs as Thumbs
 class HullThumbs(Thumbs.Thumbs):
     
     def __init__(self,shipnr,propellerDiameter=None):
-        super().__init__(shipnr,propellerDiameter=None)
+      super().__init__(shipnr,propellerDiameter=None)
+      self.linearDamping()
 
     def linearDamping(self):
-        	pi=3.14159265359
-      raddeg = 180./pi
-	RHO=1026.0  
-	grav = 9.81
+      RHO=1026.0  
+      grav = g
+      L5 = self.Lpp
+      B9 = self.beam
+      TXAFT = self.draftAft
+      TXFORE = self.draftFore
+      FMFORE = TXFORE
+      FMAFT = TXAFT
+      T9 = self.meanDraft
+      V0 = self.blockCoefficient   
+      C2 = V0  
+      V0 = self.displacement
+      COG = self.COG
+      ALW=self.waterPlaneArea
+      GYRATION = self.gyration
+      VCOB =self.verticalCenterOfBoyancy
+      IXX = GYRATION[0]**2*V0*RHO
+      IYY = GYRATION[1]**2*V0*RHO
+      GMT = self.GMT
+      GML = self.GML
+      omerol = np.sqrt(grav*V0*RHO*GMT/IXX)
+      omepit = np.sqrt(grav*V0*RHO*GML/IYY)
+      omehea = np.sqrt(ALW*grav/V0)
+      print (f'resonans periods, roll  {1/omerol*2*np.pi}, pitch {1/omepit*2*np.pi} heave {1/omehea*2*np.pi} ')
+      #	Very rough assumptions are applied here. one section shape, 
+      #	pitch equal to roll
+      #	calculate the Ap coefficients (dp assumed = 0.5)
+      DPROLL = 0.5
+      DPPITCH = 0.1*B9/L5
+      APROLL = DPROLL*(omerol**2*B9/(2.0*grav))**2
+      APPITC = DPPITCH*(omepit**2*L5/(2.0*grav))**2
+      APHEAV = 2*np.sin(omehea**3*B9/(2*grav))*np.exp(-omehea**2*T9/grav)
+      BSECROLL = RHO*grav**2/omerol**3*(B9/2)**2*APROLL**2
+      ROLLDAMP = BSECROLL*L5
+      BSECPITC = RHO*grav**2/omepit**3*(L5/2)**2*APPITC**2
+      PITCDAMP = BSECPITC*B9
+      BSECHEAV = RHO*grav**2/omehea**3*APHEAV**2
+      HEAVDAMP = BSECHEAV*L5
+      print (f'roll , pitch, heave, ',ROLLDAMP, PITCDAMP, HEAVDAMP)
+      #	NOW MAKE THE TABLES
+      #------------------------------------------------------------------------
+      #	HEAVE TABLES
+      #------------------------------------------------------------------------
+      self.HeaveDamping=np.zeros((3,2))
+      self.HeaveDamping[0,0] = -10
+      self.HeaveDamping[0,1] = 10*HEAVDAMP
 
-C Below, the necessary inputs to the damping method are collected. 
+      self.HeaveDamping[1,0] = 0
+      self.HeaveDamping[1,1] = 0
 
-	ires = 0
-      irds = 0
+      self.HeaveDamping[2,0] = 10
+      self.HeaveDamping[2,1] = -10*HEAVDAMP
+      #------------------------------------------------------------------------
+      #	PITCH TABLES
+      #------------------------------------------------------------------------
+      self.PitchDamping=np.zeros((3,2))
+      self.PitchDamping[0,0] = -1
+      self.PitchDamping[0,1] = PITCDAMP
 
-      call getvanc('<LPP>',2,IDUM,L5,ires)
-      call getvanc('<LPP>',2,IDUM,L5,ires)
-c	write(6,*)'lpp ',L5
-	call getvanc('<BEAM>',2,IDUM,B9,ires)
-C --- Dimensionalize
-	B9 = B9*L5
-c	write(6,*)'beam ',B9
-	call getvanc('<BDRAU(2)>',2,IDUM,TXAFT,ires)
-	TXAFT = TXAFT*L5
-c	write(6,*)'Draught aft ',TXAFT
-	call getvanc('<BDRAU>',2,IDUM,TXFORE,ires)
-	TXFORE = TXFORE*L5
-c	write(6,*)'Draught fore ',TXFORE
-	FMFORE = TXFORE
-	FMAFT = TXAFT
-	T9 = .5*(TXAFT+TXFORE)  ! Mean Draught
-	call getvanc('<BBLCK>'    ,2,IDUM,V0,ires)
-	C2 = V0   ! Block coefficient (Lpp)
-	V0 = V0*(L5*B9*T9)    !Displacement volume
-	call getvanc('<BCGG>'    ,2,IDUM,COG,ires)
-	COG(1) = COG(1)*L5
-	COG(3) = COG(3)*T9
-	write(6,*)'COG meters, ',COG(1),COG(3)
-C	calculate water plane area
-	call getvanc('<BALW>'    ,2,IDUM,ALW,ires)
-	ALW=ALW*L5*T9   ! water plane area
-	call getvanc('<BROG>'    ,2,IDUM,GYRATION,ires)
-	GYRATION(1) = GYRATION(1)*B9
-	GYRATION(2) = GYRATION(2)*L5
-	write(6,*)'Radii of gyration ',GYRATION(1), GYRATION(2)
-	call getvanc('<BCBG>'    ,2,IDUM,VCOB,ires)
-	VCOB =VCOB*T9
-	IXX = GYRATION(1)**2*V0*RHO
-	IYY = GYRATION(2)**2*V0*RHO
-C	call getvanc('<BAFW>'    ,2,IDUM,AFRONT,ires)
-C Now input additional DEN-Mark1 variables
-	IRDS=0
-	CALL RDS('<Enter Transverse BM in m>','>',2,1,IDUM,BMT,IRDS)
-	IRDS=0
-	CALL RDS('<Enter Longitudinal BM in m>','>',2,1,IDUM,BML,IRDS)
-C	
-C	calculate the GMT and GML: GM = KB + BM - KG
-C
-      IGMTNEG = 0
-	GMT = (T9-VCOB) + BMT - (T9 - COG(3))
-      IF ( GMT .LT. 0.0 ) THEN
-	  WRITE(6,*) 'Negative GM, the ship will capsize'
-	  IGMTNEG = 1
-	ENDIF
-	GML = (T9-VCOB) + BML - (T9 - COG(3))
-C
-c	calculate the resonance frequencies
-C
-	IF (IGMTNEG .EQ. 0 ) THEN
-	  RDUMMY = grav*V0*rho*GMT/IXX
-	  omerol = SQRT(RDUMMY)
-	ELSE
-	  omerol = 0.000001
-      ENDIF
-	RDUMMY = grav*V0*rho*GML/IYY
-	IF (RDUMMY .GT. 0.0 ) THEN
-	  omepit = SQRT(RDUMMY)
-	ELSE
-	  omepit = 0.000001
-      ENDIF
-	RDUMMY = ALW*grav/V0
-	IF (RDUMMY .GT. 0.0 ) THEN
-	  omehea = SQRT(RDUMMY)
-	ELSE
-	  omehea = 0.000001
-	ENDIF
-	write(*,*)'resonans periods, roll , pitch, heave ',
-     &             1/omerol*2*PI, 1/omepit*2*PI, 1/omehea*2*PI
+      self.PitchDamping[1,0] = 0
+      self.PitchDamping[1,1] = 0
 
-C	Very rough assumptions are applied here. one section shape, 
-C	pitch equal to roll
-C	calculate the Ap coefficients (dp assumed = 0.5)
-	DPROLL = 0.5
-C	DPPITCH = 0.05
-	DPPITCH = 0.1*B9/L5
-	APROLL = DPROLL*(omerol**2*B9/(2.0*grav))**2
-	APPITC = DPPITCH*(omepit**2*L5/(2.0*grav))**2
-	APHEAV = 2*SIN(omehea**3*B9/(2*grav))*EXP(-omehea**2*T9/grav)
+      self.PitchDamping[2,0] = 1
+      self.PitchDamping[2,1] = -PITCDAMP
+      #------------------------------------------------------------------------
+      #	ROLL TABLES
+      #------------------------------------------------------------------------
+      self.RollDamping=np.zeros((3,2))
+      self.RollDamping[0,0] = -1
+      self.RollDamping[0,1] = ROLLDAMP
 
-	BSECROLL = RHO*grav**2/omerol**3*(B9/2)**2*APROLL**2
-	ROLLDAMP = BSECROLL*L5
+      self.RollDamping[1,0] = 0
+      self.RollDamping[1,1] = 0
 
-	BSECPITC = RHO*grav**2/omepit**3*(L5/2)**2*APPITC**2
-	PITCDAMP = BSECPITC*B9
+      self.RollDamping[2,0] = 1
+      self.RollDamping[2,1] = -ROLLDAMP
+      #     ----------------------------------------------------------------
+      #	Now calculate the high frequency dampings in sway, yaw and surge
+      #     ----------------------------------------------------------------
+      self.SurgeDamping=np.zeros((3,2))
+      self.SurgeDamping[0,0] = -10
+      self.SurgeDamping[0,1] = 20
 
-	BSECHEAV = RHO*grav**2/omehea**3*APHEAV**2
-	HEAVDAMP = BSECHEAV*L5
-	WRITE(6,*)'roll , pitch, heave, ',ROLLDAMP, PITCDAMP, HEAVDAMP
-C	write(*,*)fcombn
-C	NOW MAKE THE TABLES
-C------------------------------------------------------------------------
-C	HEAVE TABLES
-C------------------------------------------------------------------------
-      NDEF1 = 3
-	ABSC1(1) = -10.0
-	ABSC1(2) =   0.0
-	ABSC1(3) =  10.0
-	YVAL(1) = ABSC1(3)*HEAVDAMP
-	YVAL(2) = 0.0
-	YVAL(3) = -ABSC1(3)*HEAVDAMP
-	
-	DO 104 I=1,3
-	  TABSC1bla(i)=0
- 104	CONTINUE
+      self.SurgeDamping[1,0] = 0
+      self.SurgeDamping[1,1] = 0
 
-C --- Generate table name
-        
-	  NOTAB = 1
-C
-        CALL coptxti('<LINEAR-HEAVE>',TABNAM)
-        CALL coptxti('<     >',TABUNA)
-        CALL coptxti('<Z_HL>',CONNAM(1,NOTAB))
-        CALL coptxti('<     >',CONUNA(1,NOTAB))
-        CALL coptxti('<W_HL>',INDNAM(1,1))
-        CALL coptxti('<>',INDUNA(1,1))
-
-        NVAR = 1
-        IBTCM = 1
-        ICTCM = 0
-        J0 = 1
-        IDUM = 1
-        FAC = 1.0
-        CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &              YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	  CALL UPDLM8
-
-        CALL coptxti('<HF-HEAVE-DAMPING>',TABNAM)
-        CALL coptxti('<     >',TABUNA)
-        CALL coptxti('<ZGHL>',CONNAM(1,NOTAB))
-        CALL coptxti('<     >',CONUNA(1,NOTAB))
-        CALL coptxti('<DWHBW>',INDNAM(1,1))
-        CALL coptxti('<>',INDUNA(1,1))
-
-        NVAR = 1
-        IBTCM = 1
-        ICTCM = 0
-        J0 = 1
-        IDUM = 1
-        FAC = 1.0
-        CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &              YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	  CALL UPDLM8
-
-C------------------------------------------------------------------------
-C	PITCH TABLES
-C------------------------------------------------------------------------
-      NDEF1 = 3
-	ABSC1(1) = -1.0
-	ABSC1(2) =   0.0
-	ABSC1(3) =  1.0
-	YVAL(1) = PITCDAMP
-	YVAL(2) = 0.0
-	YVAL(3) = -PITCDAMP
-	
-C --- Generate table name
-        
-	  NOTAB = 1
-C
-        CALL coptxti('<LINEAR-PITCH>',TABNAM)
-        CALL coptxti('<     >',TABUNA)
-        CALL coptxti('<M_HL>',CONNAM(1,NOTAB))
-        CALL coptxti('<     >',CONUNA(1,NOTAB))
-        CALL coptxti('<Q_HL>',INDNAM(1,1))
-        CALL coptxti('<>',INDUNA(1,1))
-
-        NVAR = 1
-        IBTCM = 1
-        ICTCM = 0
-        J0 = 1
-        IDUM = 1
-        FAC = 1.0
-        CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &              YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	  CALL UPDLM8
-
-        CALL coptxti('<HF-PITCH-DAMPING>',TABNAM)
-        CALL coptxti('<     >',TABUNA)
-        CALL coptxti('<MGHL>',CONNAM(1,NOTAB))
-        CALL coptxti('<     >',CONUNA(1,NOTAB))
-        CALL coptxti('<DQHBW>',INDNAM(1,1))
-        CALL coptxti('<>',INDUNA(1,1))
-
-        NVAR = 1
-        IBTCM = 1
-        ICTCM = 0
-        J0 = 1
-        IDUM = 1
-        FAC = 1.0
-        CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &              YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	  CALL UPDLM8
-
-C------------------------------------------------------------------------
-C	ROLL TABLES
-C------------------------------------------------------------------------
-      NDEF1 = 3
-	ABSC1(1) = -1.0
-	ABSC1(2) =   0.0
-	ABSC1(3) =  1.0
-	YVAL(1) = ROLLDAMP
-	YVAL(2) = 0.0
-	YVAL(3) = -ROLLDAMP
-	
-C --- Generate table name
-        
-	  NOTAB = 1
-C
-        CALL coptxti('<LINEAR-ROLL>',TABNAM)
-        CALL coptxti('<     >',TABUNA)
-        CALL coptxti('<K_HL>',CONNAM(1,NOTAB))
-        CALL coptxti('<     >',CONUNA(1,NOTAB))
-        CALL coptxti('<P_HL>',INDNAM(1,1))
-        CALL coptxti('<>',INDUNA(1,1))
-
-        NVAR = 1
-        IBTCM = 1
-        ICTCM = 0
-        J0 = 1
-        IDUM = 1
-        FAC = 1.0
-        CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &              YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	  CALL UPDLM8
-
-        CALL coptxti('<HF-ROLL-DAMPING>',TABNAM)
-        CALL coptxti('<     >',TABUNA)
-        CALL coptxti('<KGHL>',CONNAM(1,NOTAB))
-        CALL coptxti('<     >',CONUNA(1,NOTAB))
-        CALL coptxti('<DPHBW>',INDNAM(1,1))
-        CALL coptxti('<>',INDUNA(1,1))
-
-        NVAR = 1
-        IBTCM = 1
-        ICTCM = 0
-        J0 = 1
-        IDUM = 1
-        FAC = 1.0
-        CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &              YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	  CALL UPDLM8
-
-C     ----------------------------------------------------------------
-C	Now calculate the high frequency dampings in sway, yaw and surge
-C     ----------------------------------------------------------------
-      NDEF1 = 3
-	ABSC1(1) = -10.0
-	ABSC1(2) =   0.0
-	ABSC1(3) =  10.0
-	YVAL(1) = 20.0
-	YVAL(2) = 0.0
-	YVAL(3) = -20.0
-
-      CALL coptxti('<HF-SURGE-DAMPING>',TABNAM)
-      CALL coptxti('<     >',TABUNA)
-      CALL coptxti('<XGHL>',CONNAM(1,NOTAB))
-      CALL coptxti('<     >',CONUNA(1,NOTAB))
-      CALL coptxti('<DUHBW>',INDNAM(1,1))
-      CALL coptxti('<>',INDUNA(1,1))
-
-      NVAR = 1
-      IBTCM = 1
-      ICTCM = 0
-      J0 = 1
-      IDUM = 1
-      FAC = 1.0
-      CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &            YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	CALL UPDLM8
-
-      CALL coptxti('<HF-SWAY-DAMPING>',TABNAM)
-      CALL coptxti('<     >',TABUNA)
-      CALL coptxti('<YGHL>',CONNAM(1,NOTAB))
-      CALL coptxti('<     >',CONUNA(1,NOTAB))
-      CALL coptxti('<DVHBW>',INDNAM(1,1))
-      CALL coptxti('<>',INDUNA(1,1))
-
-      NVAR = 1
-      IBTCM = 1
-      ICTCM = 0
-      J0 = 1
-      IDUM = 1
-      FAC = 1.0
-      CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &            YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	CALL UPDLM8
-
-      NDEF1 = 3
-	ABSC1(1) = -10.0
-	ABSC1(2) =   0.0
-	ABSC1(3) =  10.0
-	YVAL(1) = 10.0
-	YVAL(2) = 0.0
-	YVAL(3) = -10.0
-
-      CALL coptxti('<HF-YAW-DAMPING>',TABNAM)
-      CALL coptxti('<     >',TABUNA)
-      CALL coptxti('<NGHL>',CONNAM(1,NOTAB))
-      CALL coptxti('<     >',CONUNA(1,NOTAB))
-      CALL coptxti('<DRHBW>',INDNAM(1,1))
-      CALL coptxti('<>',INDUNA(1,1))
-
-      NVAR = 1
-      IBTCM = 1
-      ICTCM = 0
-      J0 = 1
-      IDUM = 1
-      FAC = 1.0
-      CALL MKTABL(IBTCM,ICTCM,J0,NDEF1,IDUM,absc1,RDUM,tabsc1bla,IDUM,
-     &            YVAL,FAC)
-C --- UPDLM8 updates the lump with the modified table
-	CALL UPDLM8
-
-C	Update the hydrstatic common area for later printout of hydrostatic properties
+      self.SurgeDamping[2,0] = 10
+      self.SurgeDamping[2,1] = -20
       
-	KEELGRAV = (T9 - COG(3))
-	KEELBUOY = (T9-VCOB)
-	BUOMETTR = BMT
-	BUOMETLO = BML
-	METCENTR = GMT
-	METCENLO = GML
-	INERIAXX = IXX
-	INERIAYY = IYY
-	PERIODRO = 1/omerol*2*PI 
-	PERIODPI = 1/omepit*2*PI
-      PERIODHE = 1/omehea*2*PI
+
+      self.SwayDamping=np.zeros((3,2))
+      self.SwayDamping[0,0] = -10
+      self.SwayDamping[0,1] = 0
+
+      self.SwayDamping[1,0] = 0
+      self.SwayDamping[1,1] = 0
+
+      self.SwayDamping[2,0] = 10
+      self.SwayDamping[2,1] = -10      
+
+    def getHeaveDamping(self):
+      return self.HeaveDamping
+    def getPitchDamping(self):
+      return self.PitchDamping
+    def getRollDamping(self):
+      return self.RollDamping
+    def getSurgeDamping(self):
+      return self.SurgeDamping
+    def getSwayDamping(self):
+      return self.HeaveDamping
--- src/Thumbs.py
+++ src/Thumbs.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 
 class Thumbs:
 
@@ -8,9 +9,12 @@ class Thumbs:
         self.read_SY1Data(shipnr)
         print('hardcoded some values here for now')
         self.lengthWaterLine = self.ship_data['Length_of_Waterline'] 
+        self.Lpp = 312 # From ShipData
         self.beam = 53.5  ## get from shipDataLib
         self.pRatio = self.ship_data['Propeller_Area_Ratio']
         self.wettedSurface = 23243.8  ## get from shipDataLib
+        self.waterPlaneArea = 0.893628117 * self.Lpp * self.beam
+       
         self.PD = 0.71505  ## get from shipDataLib
         self.nrProp = self.ship_data['number_of_propellers']
         self.nrBlades = self.ship_data['number_of_blades']
@@ -26,6 +30,10 @@ class Thumbs:
         self.nozzle = 0
         self.meanDraft =(self.draftAft + self.draftFore)/2.0
         self.waterLineBlock = self.displacement /(self.lengthWaterLine*self.beam*self.meanDraft) # waterline block coefficient
+        self.blockCoefficient = 0.704661757
+        self.COG=np.array([-0.002290749, 0, -0.415058824]) * np.array([self.Lpp ,self.beam,self.meanDraft])
+        self.verticalCenterOfBoyancy = 0.453647058824 * self.meanDraft
+        self.gyration = np.array([0.4, 0.25, 0.25]) * np.array([self.beam,self.Lpp,self.Lpp])
     
        
         self.Formal = self.ship_data['Total_FormFactor'] ## 1.21268
@@ -36,6 +44,10 @@ class Thumbs:
         self.verticalCenterBulb = self.ship_data['verticalCenterBulb']## 5.0 #Vertical position of center of bulb from BL, m 
         self.LCB_ratio = self.ship_data['LCB_ratio'] ##0.00612186
         self.Fmxaft = self.ship_data['FormFactorStern']
+
+        self.GMT = self.ship_data['GMT']
+        self.GML = self.ship_data['GML']
+
 
         #self.areaRatio = aRatio
        # self.Displacement = 100000
